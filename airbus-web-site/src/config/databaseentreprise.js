487687c0d58699fe1a1d30bee2b6b1781a068f8f
Mathéo Martin <matheo.martin@epitech.eu>
2022-05-31 14:21:18 +0200

    [Upload] add the database connection

---
+++ airbus-web-site/src/config/databaseentreprise.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql2');
+require('dotenv').config();
+
+class DatabaseEntreprise {
+    constructor(config) {
+        this.connection = mysql.createPool({
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE_ENTREPRISE,
+            connectionLimit: 10
+        });
+    }
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) => {
+                if (err) return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+    close() {
+        return new Promise((resolve, reject) => {
+            this.connection.end(err => {
+                if (err) return reject(err);
+                resolve();
+            });
+        });
+    }
+}
+
+module.exports = new databaseEntreprise();
