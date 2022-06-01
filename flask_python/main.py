15119a40f5a56aa11c30096a352d99509529b01f
Antonin Laudon <antonin.laudon@epitech.eu>
2022-06-01 23:26:02 +0200

    rick?

--- flask_python/main.py
+++ flask_python/main.py
@@ -24,4 +24,28 @@ def suppliers():
 def main():
     return flask.render_template("main.html")
 
+@app.route("/contact")
+def contact():
+    return flask.render_template("contact.html")
+
+@app.route("/cookie")
+def cookie():
+    return flask.render_template("cookie.html")
+
+@app.route("/log-in")
+def login():
+    return flask.render_template("log-in.html")
+
+@app.route("/map")
+def map():
+    return flask.render_template("map.html")
+
+@app.route("/privacy")
+def privacy():
+    return flask.render_template("privacy.html")
+
+@app.route("/register")
+def register():
+    return flask.render_template("register.html")
+
 app.run("127.0.0.1", 3945, debug=True)
